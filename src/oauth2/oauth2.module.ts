9bc39cbb45406fc5e6d75f38c2eafc83c1458a2f
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-01-03 01:33:58 +0530

    change in token claims

--- src/oauth2/oauth2.module.ts
+++ src/oauth2/oauth2.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { LogModule } from "../common/modules/log/log.module";
 import { PrismaModule } from "../common/modules/prisma/prisma.module";
 import { OAuth2Controller } from "./oauth2.controller";
 import { OAuth2Service } from "./oauth2.service";
 
 @Module({
-  imports: [JwtModule.register({}), PrismaModule],
+  imports: [JwtModule.register({}), PrismaModule, LogModule],
   controllers: [OAuth2Controller],
   providers: [OAuth2Service],
 })
