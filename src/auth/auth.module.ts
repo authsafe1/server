4cad49caf03a5ae55220492b7a4e4bfda4939076
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-01-02 13:51:15 +0530

    strategy location changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,9 @@ import { PassportModule } from "@nestjs/passport";
 import { ActivityLogService } from "../common/modules/log/activity-log.service";
 import { PrismaModule } from "../common/modules/prisma/prisma.module";
 import { QueueModule } from "../common/modules/queue/queue.module";
+import { GoogleStrategy } from "../common/strategy/google.strategy";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { GoogleStrategy } from "./google.strategy";
 
 @Module({
   imports: [
