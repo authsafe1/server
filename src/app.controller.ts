5dc82dedc829233b221d60637592190407c00af3
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-01-07 20:36:44 +0530

    root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from "@nestjs/common";
 @Controller("")
 export class AppController {
   @Get("")
-  getRoot() {
+  root() {
     return { message: "Server is up and running" };
   }
 }
