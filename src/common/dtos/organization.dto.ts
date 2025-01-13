a1287a9159c7a6ad0501ddcbae45cdfb79452d14
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-01-14 00:05:12 +0530

    dto changes

--- src/common/dtos/organization.dto.ts
+++ src/common/dtos/organization.dto.ts
@@ -1,11 +1,46 @@
+import { Prisma } from "@prisma/client";
 import { JsonValue } from "@prisma/client/runtime/library";
-import { Transform } from "class-transformer";
-import { IsObject, IsOptional, IsString, Matches } from "class-validator";
+import { Transform, Type } from "class-transformer";
+import {
+  IsInt,
+  IsObject,
+  IsOptional,
+  IsString,
+  Matches,
+  Min,
+} from "class-validator";
+
+export class OrganizationsDto {
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Type(() => Number)
+  skip?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  take?: number;
+
+  @IsOptional()
+  @IsObject()
+  cursor?: Prisma.OrganizationWhereUniqueInput;
+
+  @IsOptional()
+  @IsObject()
+  where?: Prisma.OrganizationWhereInput;
+
+  @IsOptional()
+  @IsObject()
+  orderBy?: Prisma.OrganizationOrderByWithRelationInput;
+}
 
 export class CreateOrganizationDto {
   @IsString()
   name: string;
 
+  @IsString()
   @Matches(
     /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/g,
     { message: "Must be a valid domain name" },
