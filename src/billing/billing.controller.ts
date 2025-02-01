6fd09d02bbd50c3bb3a17e58afb42101119f8a88
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-02-02 02:12:42 +0530

    new razorpay plans

--- src/billing/billing.controller.ts
+++ src/billing/billing.controller.ts
@@ -1,37 +1,45 @@
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
 import {
   Body,
   Controller,
-  Param,
+  Inject,
   Post,
   Put,
   Session,
   UseGuards,
 } from "@nestjs/common";
+import { Cache } from "cache-manager";
 import { Request } from "express";
 import {
   CreateBillingDto,
   VerifyPaymentDto,
 } from "src/common/dtos/billing.dto";
+import { CacheInvalidate } from "../common/decorators/cache.decorator";
 import { EnsureLoginGuard } from "../common/guards/ensure-login.guard";
 import { BillingService } from "./billing.service";
 
-@UseGuards(EnsureLoginGuard)
 @Controller("billing")
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  constructor(
+    private readonly billingService: BillingService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+  ) {}
 
+  @UseGuards(EnsureLoginGuard)
   @Post("create")
   async createSubscription(
     @Body() dto: CreateBillingDto,
     @Session() session: Request["session"],
   ) {
     return this.billingService.createSubscription(
-      session.profile.id,
-      dto.planId,
+      dto.type,
+      session?.profile?.id,
     );
   }
 
+  @UseGuards(EnsureLoginGuard)
   @Post("verify")
+  @CacheInvalidate("isAuthenticated")
   async verifyPayment(
     @Body() dto: VerifyPaymentDto,
     @Session() session: Request["session"],
@@ -40,12 +48,14 @@ export class BillingController {
       dto.paymentId,
       dto.subscriptionId,
       dto.razorpaySignature,
-      session.profile.id,
+      session?.profile?.id,
     );
   }
 
-  @Put(":subscriptionId/cancel")
-  async cancelSubscription(@Param("subscriptionId") subscriptionId: string) {
-    return this.billingService.cancelSubscription(subscriptionId);
+  @UseGuards(EnsureLoginGuard)
+  @Put("cancel")
+  @CacheInvalidate("isAuthenticated")
+  async cancelSubscription(@Session() session: Request["session"]) {
+    return this.billingService.cancelSubscription(session?.profile?.id);
   }
 }
--- src/billing/billing.service.ts
+++ src/billing/billing.service.ts
@@ -1,10 +1,10 @@
 import {
+  BadRequestException,
   Injectable,
   InternalServerErrorException,
   Logger,
-  UnauthorizedException,
 } from "@nestjs/common";
-import { SubscriptionStatus } from "@prisma/client";
+import { SubscriptionStatus, SubscriptionType } from "@prisma/client";
 import { createHmac } from "crypto";
 import dayjs from "dayjs";
 import Razorpay from "razorpay";
@@ -22,28 +22,23 @@ export class BillingService {
     });
   }
 
-  async createSubscription(profileId: string, planId: string) {
+  async createSubscription(
+    type: "PROFESSIONAL" | "ENTERPRISE",
+    profileId: string,
+  ) {
     try {
+      let planId = "";
+      if (type === "PROFESSIONAL") {
+        planId = process.env.RAZORPAY_PLAN_PROFESSIONAL;
+      } else if (type === "ENTERPRISE") {
+        planId = process.env.RAZORPAY_PLAN_ENTERPRISE;
+      }
       const subscription = await this.razorpay.subscriptions.create({
         plan_id: planId,
         customer_notify: 1,
         total_count: 12,
         start_at: dayjs().add(1, "day").unix(),
         notes: { profileId },
-      });
-
-      await this.prismaService.subscription.create({
-        data: {
-          subscriptionId: subscription.id,
-          status: SubscriptionStatus.INCOMPLETE,
-          startDate: dayjs().add(1, "day").toDate(),
-          endDate: dayjs().add(12, "months").toDate(),
-          Profile: {
-            connect: {
-              id: profileId,
-            },
-          },
-        },
       });
 
       return { subscriptionId: subscription.id };
@@ -58,33 +53,64 @@ export class BillingService {
     razorpaySignature: string,
     profileId: string,
   ) {
-    const generatedSignature = createHmac(
-      "sha256",
-      process.env.RAZORPAY_KEY_SECRET,
-    )
-      .update(`${paymentId}|${subscriptionId}`)
-      .digest("hex");
-    if (generatedSignature === razorpaySignature) {
-      await this.prismaService.subscription.update({
-        where: {
-          subscriptionId,
-          profileId,
-        },
-        data: {
-          status: SubscriptionStatus.ACTIVE,
-        },
-      });
-      return { message: "Payment verified" };
-    } else {
-      throw new UnauthorizedException("Payment failed");
+    try {
+      const generatedSignature = createHmac(
+        "sha256",
+        process.env.RAZORPAY_KEY_SECRET,
+      )
+        .update(`${paymentId}|${subscriptionId}`)
+        .digest("hex");
+      const subscription =
+        await this.razorpay.subscriptions.fetch(subscriptionId);
+
+      let planType: SubscriptionType = SubscriptionType.FREE;
+
+      if (subscription.plan_id === process.env.RAZORPAY_PLAN_PROFESSIONAL) {
+        planType = SubscriptionType.PROFESSIONAL;
+      } else if (
+        subscription.plan_id === process.env.RAZORPAY_PLAN_ENTERPRISE
+      ) {
+        planType = SubscriptionType.ENTERPRISE;
+      }
+
+      if (generatedSignature === razorpaySignature) {
+        await this.prismaService.subscription.update({
+          where: {
+            profileId,
+          },
+          data: {
+            subscriptionId,
+            type: planType,
+            startDate: dayjs().add(1, "day").toDate(),
+            endDate: dayjs().add(12, "months").toDate(),
+            status: SubscriptionStatus.ACTIVE,
+          },
+        });
+        return { message: "Payment verified" };
+      } else {
+        throw new BadRequestException("Payment failed");
+      }
+    } catch {
+      throw new InternalServerErrorException("Payment failed");
     }
   }
 
-  async cancelSubscription(subscriptionId: string) {
-    await this.razorpay.subscriptions.cancel(subscriptionId);
+  async cancelSubscription(profileId: string) {
+    const subscription = await this.prismaService.subscription.findUnique({
+      where: {
+        profileId,
+      },
+    });
+    await this.razorpay.subscriptions.cancel(subscription.subscriptionId);
     await this.prismaService.subscription.update({
-      where: { subscriptionId },
-      data: { status: SubscriptionStatus.CANCELLED },
+      where: { profileId },
+      data: {
+        type: SubscriptionType.FREE,
+        status: SubscriptionStatus.CANCELLED,
+        startDate: dayjs().toDate(),
+        endDate: dayjs().add(100, "years").toDate(),
+        subscriptionId: `free-${profileId}`,
+      },
     });
 
     return { success: true, message: "Subscription cancelled" };
