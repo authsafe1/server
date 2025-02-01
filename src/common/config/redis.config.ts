29a69c00744479906a12ec1d3d7b3796d931d56c
Anapeksha Mukherjee <46746006+anapeksha@users.noreply.github.com>
2025-02-02 02:12:56 +0530

    retry logic and exponential backoff

--- src/common/config/redis.config.ts
+++ src/common/config/redis.config.ts
@@ -3,8 +3,16 @@ import { Redis } from "ioredis";
 
 config();
 
-export const redis = new Redis(
-  Number(process.env.REDIS_PORT),
-  process.env.REDIS_HOST,
-  { password: process.env.REDIS_PASSWORD },
-);
+export const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD || null,
+  retryStrategy: times => {
+    console.log(`Redis connection lost. Retrying attempt ${times}...`);
+    return Math.min(times * 100, 3000);
+  },
+  reconnectOnError: err => {
+    console.error("Redis error:", err);
+    return true;
+  },
+});
